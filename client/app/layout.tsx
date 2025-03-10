7fb8cbea84ca8a16cbaa1debb39533282c9d087d
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2025-03-10 11:12:51 +0530

    added analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 import {AuthProvider} from "@/hooks/useAuth"
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       > 
           <AuthProvider>
             {children}
+            <Analytics />
             <Toaster />
           </AuthProvider>
       </body>
